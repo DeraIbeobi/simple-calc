b34d84b4d3c62645d2e806203e5ae3da89d942a8
DeraIbeobi <deraibeobi@gmail.com>
2023-05-06 23:35:38 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter the first number ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Enter the Second number ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a maths operation  (+, -, *, /): ");
+            string operation = Console.ReadLine();
+
+            double result = 0;
+
+            switch(operation)
+            {
+                case "+":
+                result = num1 + num2;
+                break;
+                case "-":
+                result = num1 - num2;
+                break;
+                case "*":
+                result = num1 * num2;
+                break;
+                case "/":
+                result = num1 / num2;
+                break;
+                default:
+                System.Console.WriteLine("Invalid entry.");
+                break;
+            }
+            System.Console.WriteLine("Result = " + result);
+            Console.ReadKey();
+        }
+    }
+}
